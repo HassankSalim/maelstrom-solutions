392e4aeb853de6bf46c7480c2172bb3bc2302453
Hassan K Salim <hassanksalim@gmail.com>
2023-12-17 22:37:45 +0530

    Get neighbours from topology RPC

--- maelstrom-broadcast/main.go
+++ maelstrom-broadcast/main.go
@@ -11,10 +11,14 @@ import (
 func main() {
 	n := maelstrom.NewNode()
 	messages := []float64{}
+	neigbhourNodes := []string{}
 
 	n.Handle("broadcast", func(msg maelstrom.Message) error {
+		logger := log.New(os.Stderr, "", log.Ltime)
+
 		// Unmarshal the message body as an loosely-typed map.
 		var body map[string]any
+
 		if err := json.Unmarshal(msg.Body, &body); err != nil {
 			return err
 		}
@@ -25,6 +29,14 @@ func main() {
 		messages = append(messages, messageVal)
 		// Update the message type to return back.
 		body["type"] = "broadcast_ok"
+
+		gossipReqBody := map[string]interface{}{
+			"type":    "broadcast",
+			"message": messageVal,
+		}
+		for _, neighbour := range neigbhourNodes {
+			n.RPC(neighbour, gossipReqBody, func(msg maelstrom.Message) error { logger.Printf("Message sent to %s", neighbour); return nil })
+		}
 		delete(body, "message")
 
 		// Echo the original message back with the updated message type.
@@ -47,6 +59,7 @@ func main() {
 	})
 
 	n.Handle("topology", func(msg maelstrom.Message) error {
+		logger := log.New(os.Stderr, "", log.Ltime)
 		// Unmarshal the message body as an loosely-typed map.
 		var body map[string]any
 		if err := json.Unmarshal(msg.Body, &body); err != nil {
@@ -54,6 +67,17 @@ func main() {
 		}
 
 		// Update the message type to return back.
+		nodeID := n.ID()
+		topology, _ := body["topology"].(map[string]interface{})
+
+		neighbourVals, _ := topology[nodeID].([]interface{})
+		for _, neighbourVal := range neighbourVals {
+			if val, ok := neighbourVal.(string); ok {
+				neigbhourNodes = append(neigbhourNodes, val)
+			}
+		}
+		logger.Printf("Node %s has Neigbhours: %s", nodeID, neigbhourNodes)
+
 		body["type"] = "topology_ok"
 		delete(body, "topology")
 
